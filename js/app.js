62ac529de1435ca6aaa9a02b69ad14228b20154e
Guilherme Guerra <guerrinha@comum.org>
2016-12-21 00:30:51 -0200

    Add org parse on preview

--- js/app.js
+++ js/app.js
@@ -3,8 +3,21 @@ window.onload = function() {
   var editor = document.getElementById("editor");
   var preview = document.getElementById("preview");
 
+
+
+
   editor.addEventListener('input', function(e) {
-    preview.innerHTML = e.target.value;
+    var orgParser = new Org.Parser();
+    var orgDocument = orgParser.parse(e.target.value);
+
+    var orgHTMLDocument = orgDocument.convert(Org.ConverterHTML, {
+      headerOffset: 1,
+      exportFromLineNumber: false,
+      suppressSubScriptHandling: false,
+      suppressAutoLink: false
+    });
+
+    preview.innerHTML = orgHTMLDocument.toString();
   }, false);
 
 }
